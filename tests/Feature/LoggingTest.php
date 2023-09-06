eeb0ea08cdf68c3b0249d8152e79209c17831d46
galihroswandi <galihroswandi25@gmail.com>
2023-09-06 08:59:58 +0700

    feat: Context

--- tests/Feature/LoggingTest.php
+++ tests/Feature/LoggingTest.php
@@ -18,4 +18,18 @@ class LoggingTest extends TestCase
 
         self::assertTrue(true);
     }
+
+    public function testWithContext()
+    {
+        // Log::info("Hello Info", ['user' => "jhondoe"]);
+        // Log::warning("Hello Warning", ['user' => 'jhondoe']);
+
+        Log::withContext(['user' => 'jhondoe']);
+        Log::info("Hello Info");
+        Log::warning("Hello Warning");
+        Log::error("Hello Error");
+        Log::critical("Hello Critical");
+
+        self::assertTrue(true);
+    }
 }
